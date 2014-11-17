ca4589974f0f67577b4bc520e2312ceaa1c9c694
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-11-16 20:23:40 -0800

    Restructure Keys data type

--- Keys.elm
+++ Keys.elm
@@ -1,50 +1,55 @@
-module Keys (KeyInput(..), lastPressed) where
+module Keys (Key(..), KeyCombo(..), lastPressed) where
 
 import Signal
 import Native.Keys
 
-data KeyInput =
-  Enter | Backspace |
-  Left | Right | Up | Down |
-  Character String |
-  Command String |
-  Unrecognized String
+data Key
+  = Enter | Backspace
+  | Left | Right | Up | Down
 
-fromPresses : String -> KeyInput
+data KeyCombo
+  = Single Key
+  | Character String
+  | Command Key
+  | CommandCharacter String
+  | Shift Key
+  | Unrecognized String
+
+fromPresses : String -> KeyCombo
 fromPresses string = case string of
-  "\r" -> Enter
+  "\r" -> Single Enter
   _ -> Character string
 
-fromDowns : Int -> KeyInput
+fromDowns : Int -> KeyCombo
 fromDowns code = case code of
-  8 -> Backspace
-  13 -> Enter
-  37 -> Left
-  38 -> Up
-  39 -> Right
-  40 -> Down
+  8 ->  Single Backspace
+  13 -> Single Enter
+  37 -> Single Left
+  38 -> Single Up
+  39 -> Single Right
+  40 -> Single Down
   _ -> Unrecognized (show code)
 
-fromMeta : Int -> KeyInput
+fromMeta : Int -> KeyCombo
 fromMeta code = case code of
-  37 -> Command "Left"
-  38 -> Command "Up"
-  39 -> Command "Right"
-  40 -> Command "Down"
-  49 -> Command "1"
-  50 -> Command "2"
-  51 -> Command "3"
-  52 -> Command "4"
-  53 -> Command "5"
-  54 -> Command "6"
-  55 -> Command "7"
-  65 -> Command "a"
-  68 -> Command "d"
-  77 -> Command "m"
-  80 -> Command "p"
+  37 -> Command Left
+  38 -> Command Up
+  39 -> Command Right
+  40 -> Command Down
+  49 -> CommandCharacter "1"
+  50 -> CommandCharacter "2"
+  51 -> CommandCharacter "3"
+  52 -> CommandCharacter "4"
+  53 -> CommandCharacter "5"
+  54 -> CommandCharacter "6"
+  55 -> CommandCharacter "7"
+  65 -> CommandCharacter "a"
+  68 -> CommandCharacter "d"
+  77 -> CommandCharacter "m"
+  80 -> CommandCharacter "p"
   _ -> Unrecognized ("Meta-" ++ show code)
 
-lastPressed : Signal KeyInput
+lastPressed : Signal KeyCombo
 lastPressed = Signal.merges
   [ fromPresses <~ Native.Keys.pressesIn
   , fromDowns <~ Native.Keys.downsIn
